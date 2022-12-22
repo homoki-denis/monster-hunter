8b9673dd9720909b9939d2148e8b6c3cb3afd35f
Denis Homoki <homoki.denisth@gmail.com>
2022-12-22 13:12:09 +0200

    added game start method

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      gameIsRunning: false,
       playerHealth: 100,
       monsterHealth: 100,
     };
+  },
+  methods: {
+    startGame() {
+      this.gameIsRunning = !this.gameIsRunning;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
   },
 }).mount("#app");
