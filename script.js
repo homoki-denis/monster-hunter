06168c91ea57149729d186b3d0ee560b8ff1e648
Denis Homoki <homoki.denisth@gmail.com>
2022-12-22 15:32:30 +0200

    added all buttons

--- script.js
+++ script.js
@@ -14,5 +14,42 @@ createApp({
       this.playerHealth = 100;
       this.monsterHealth = 100;
     },
+    calculateDamage(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin() {
+      if (this.monsterHealth <= 0) {
+        confirm("You Won! Want to play again?");
+        this.startGame();
+      } else if (this.playerHealth <= 0) {
+        confirm("You Lost! Want to play again?");
+        this.startGame();
+      }
+    },
+    attack() {
+      this.monsterHealth -= this.calculateDamage(2, 10);
+      this.checkWin();
+
+      this.playerHealth -= this.calculateDamage(3, 12);
+      this.checkWin();
+    },
+    specialAttack() {
+      this.monsterHealth -= this.calculateDamage(10, 20);
+      this.checkWin();
+
+      this.playerHealth -= this.calculateDamage(3, 12);
+      this.checkWin();
+    },
+    heal() {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+      this.playerHealth -= this.calculateDamage(3, 12);
+    },
+    giveUp() {
+      this.startGame();
+    },
   },
 }).mount("#app");
